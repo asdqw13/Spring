c0970937043df076d67f2a1aa000cfdd2eed4cae
asdqw13 <asdqw13@naver.com>
2020-10-05 13:46:12 +0900

    1005

---
+++ spring_source/a10_5_Intercepter/WebContent/WEB-INF/views/index.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<h1>index</h1>
+	<a href='user/modify'>user/modify</a>
+	<br />
+	<a href='user/logout'>user/logout</a>
+	<br />
+	<a href='board/write'>board/write</a>
+	<br />
+	<a href='board/main'>board/main</a>
+	<br />
+	<a href='board/modify'>board/modify</a>
+	<br />
+	<a href='board/delete'>board/delete</a>
+	<br />
+	<hr />
+	<h2>예외처리</h2>
+	<a href='test1'>test1 배열크기 에러</a>
+	<br />
+	<a href='test2'>test2 널값 에러</a>
+	<br />
+	<hr />
+	<h2>restController 테스트</h2>
+	<a href='test3'>test3 파일로 이동하는 것이 아님</a>
+	<br />
+	<a href='test4'>test4 파일로 이동하는 것이 아님</a>
+	<br />
+</body>
+</html>
---
+++ spring_source/a10_5_Intercepter/src/kr/co/korea/intercepter/TestLoginInterceptor.java
@@ -0,0 +1,34 @@
+package kr.co.korea.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class TestLoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		System.out.println("-----------------------------");
+		System.out.println("(1)preHandle Login Test");
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		
+		System.out.println("(3)postHandle Login test");
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		
+		System.out.println("(5)afterCompletion Login test");
+		System.out.println("-----------------------------");
+	}
+}
